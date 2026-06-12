d336cc51701c37184cea0eb81e7b221c8ef22891
alexandertuna <alexander.tuna@gmail.com>
2026-06-11 21:07:21 -0700

    Adjust the run script for the new layers pardigm

--- run/run.sh
+++ run/run.sh
@@ -1,4 +1,5 @@
 # Example of how to run signal muonGun
-maia_doublets --outer --geo v01 --signal --sim
+maia_doublets --geo v01 --signal --sim # --plot
 
 # Example of how to run background neutrinoGun
+maia_doublets --geo v01 --background100 --sim # --plot
