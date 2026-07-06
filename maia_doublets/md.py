abee429696ba57ae176ef5090ac38b496e95a65f
alexandertuna <alexander.tuna@gmail.com>
2026-07-06 11:20:29 -0700

    No need for this anymore

--- maia_doublets/md.py
+++ maia_doublets/md.py
@@ -20,7 +20,6 @@ class MDMaker:
         simhits: pd.DataFrame,
     ):
         self.signal = signal
-        self.cutflow = None
         self.cut_mds = cut_mds
         self.MD_DZ_CUT = calibs.get("doublet_dz", np.zeros((10, 10)))
         self.MD_DR_CUT = calibs.get("doublet_dr", np.zeros((10, 10)))
