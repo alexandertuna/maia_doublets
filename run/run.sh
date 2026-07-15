cfdc78eb6fec39601c33901d3c8b541aa96e667a
alexandertuna <alexander.tuna@gmail.com>
2026-07-14 18:00:40 -0700

    Expand this a bit

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,27 @@
-# Example of how to run signal muonGun
-maia_doublets --geo v01 --signal --sim # --plot
+# How to calibrate cut thresholds (3sigma intervals)
+# maia_doublets --geo v01 --signal --digi --smear 10um --calibrate
 
-# Example of how to run background neutrinoGun
-maia_doublets --geo v01 --background100 --sim # --plot
+# How to make plots
+# maia_doublets --geo v01 --signal --digi --smear 10um --plot
+
+# How to find the overall efficiency
+# maia_doublets --geo v01 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
+
+# How to plot the 0-10 GeV muonGun sample
+# maia_doublets --geo v01 -i "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_0_10/10um/muonGun_pT_0_10_digi_3*" --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
+
+# How to run background neutrinoGun
+# GEO="v01"
+# SMEAR="10um"
+# OUTDIR=${GEO}_background100_digi_${SMEAR}
+# maia_doublets \
+#   --geo ${GEO} \
+#   --background100 \
+#   --digi \
+#   --smear ${SMEAR} \
+#   --fast-mds \
+#   --cutflow ${OUTDIR}/cutflow.ndjson \
+#   --write-mcps ${OUTDIR}/mcps.pkl \
+#   --write-simhits ${OUTDIR}/simhits.pkl \
+#   --write-mds ${OUTDIR}/mds.pkl # --plot
+
