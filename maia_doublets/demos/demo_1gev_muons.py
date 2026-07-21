7a0af1c61962717e89bd558b6cfb11b467232a4c
alexandertuna <alexander.tuna@gmail.com>
2026-07-21 16:36:47 -0700

    Make it even prettier

--- maia_doublets/demos/demo_1gev_muons.py
+++ maia_doublets/demos/demo_1gev_muons.py
@@ -34,20 +34,19 @@ COLLECTIONS = [
 ]
 MUON = 13
 MAX_ETA = 0.65
-N_PLOTS = 50
+N_PLOTS = 100
 FNAME = "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_1p0_1p1/10um/muonGun_pT_1p0_1p1_sim_300.slcio"
 RADII = [
     127, 167, 510, 550,
     819, 899, 1366, 1446,
 ]
-R_MAX = max(RADII) + 1.02
+R_MAX = max(RADII) * 1.02
 
 def main():
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s [%(levelname)s] %(message)s")
     df = slcio_to_df(FNAME)
-    print(df)
-    with PdfPages("hits_per_mcp.pdf") as pdf:
+    with PdfPages("one_gev_muons.pdf") as pdf:
         plot_hits_per_mcp(df, pdf)
 
 
@@ -66,20 +65,25 @@ def plot_hits_per_mcp(df: pd.DataFrame, pdf: PdfPages):
         n_hits = len(group)
         logger.info(f"Event {i_event}, MCP {i_mcp}: {n_hits} hits")
         fig, ax = plt.subplots()
+        circles = [plt.Circle((0,0), rad, **cargs) for rad in RADII]
+        for circle in circles:
+            ax.add_patch(circle)
         ax.scatter(group["hit_x"],
                    group["hit_y"],
                    s=20,
                    c="red",
                    edgecolors="black",
                    )
-        ax.set_title(f"Event {i_event}, MCP {i_mcp}: {n_hits} hits")
+        mcp_pt = group["mcp_pt"].iloc[0]
+        mcp_eta = group["mcp_eta"].iloc[0]
+        mcp_phi = group["mcp_phi"].iloc[0]
+        mcp_props = f"pT: {mcp_pt:.2f} GeV\neta: {mcp_eta:.2f}\nphi: {mcp_phi:.2f}"
+        ax.text(0.02, 0.98, mcp_props, transform=ax.transAxes, va="top", ha="left")
+        ax.set_title(f"Event {i_event}, MCP {i_mcp}, : {n_hits} hits")
         ax.set_xlabel("x [mm]")
         ax.set_ylabel("y [mm]")
         ax.set_xlim(-R_MAX, R_MAX)
         ax.set_ylim(-R_MAX, R_MAX)
-        circles = [plt.Circle((0,0), rad, **cargs) for rad in RADII]
-        for circle in circles:
-            ax.add_patch(circle)
         pdf.savefig(fig)
         plt.close(fig)
         if i_group >= N_PLOTS:
@@ -111,6 +115,7 @@ def slcio_to_df(slcio_file_path):
                 mcp_pt = np.sqrt(mcp_px**2 + mcp_py**2)
                 mcp_theta = np.arctan2(mcp_pt, mcp_pz)
                 mcp_eta = -np.log(np.tan(mcp_theta / 2))
+                mcp_phi = np.arctan2(mcp_py, mcp_px)
                 if np.abs(mcp_eta) > MAX_ETA:
                     continue
 
@@ -123,6 +128,9 @@ def slcio_to_df(slcio_file_path):
                     "hit_x": position[0],
                     "hit_y": position[1],
                     "hit_z": position[2],
+                    "mcp_pt": mcp_pt,
+                    "mcp_eta": mcp_eta,
+                    "mcp_phi": mcp_phi,
                 })
 
     logger.info(f"Processed {len(hits)} hits in {i_event + 1} events.")
