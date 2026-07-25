5030e7a36998a86b3f43488b25ffba2f5d6e2b7f
alexandertuna <alexander.tuna@gmail.com>
2026-07-24 20:59:21 -0700

    Adding commands for various checks

--- run/run.sh
+++ run/run.sh
@@ -2,24 +2,32 @@
 # maia_doublets --geo v01 --signal --digi --smear 10um --calibrate
 
 # How to make plots
-GEO="v01"
-SMEAR="10um"
-OUTDIR=${GEO}_signal_digi_${SMEAR}
-maia_doublets \
-    --geo ${GEO} \
-    --signal \
-    --digi \
-    --smear ${SMEAR} \
-    --write-simhits ${OUTDIR}/hits.pkl \
-    --write-mcps ${OUTDIR}/mcps.pkl \
-    --write-mds ${OUTDIR}/mds.pkl \
-    --write-t2s ${OUTDIR}/t2s.pkl \
-    --write-t4s ${OUTDIR}/t4s.pkl \
-    --write-t8s ${OUTDIR}/t8s.pkl \
-    --plot
+# GEO="v01"
+# SMEAR="10um"
+# OUTDIR=${GEO}_signal_digi_${SMEAR}
+# maia_doublets \
+#     --geo ${GEO} \
+#     --signal \
+#     --digi \
+#     --smear ${SMEAR} \
+#     --plot
+#     # --write-simhits ${OUTDIR}/hits.pkl \
+#     # --write-mcps ${OUTDIR}/mcps.pkl \
+#     # --write-mds ${OUTDIR}/mds.pkl \
+#     # --write-t2s ${OUTDIR}/t2s.pkl \
+#     # --write-t4s ${OUTDIR}/t4s.pkl \
+#     # --write-t8s ${OUTDIR}/t8s.pkl \
+#     # --cut-mds \
+#     # --cut-t2s \
+#     # --cut-t4s \
+#     # --cut-t8s
+#     # --plot
 
 # How to find the overall efficiency
 # maia_doublets --geo v01 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
+  
+# How to plot the 0-10 GeV pionGun sample
+maia_doublets --geo v01 -i "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/pionGun_pT_0_10/10um/pionGun_pT_0_10_digi_30*" --digi --smear 10um --plot # --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
 
 # How to plot the 0-10 GeV muonGun sample
 # maia_doublets --geo v01 -i "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_0_10/10um/muonGun_pT_0_10_digi_3*" --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
@@ -39,3 +47,15 @@ maia_doublets \
 #   --write-simhits ${OUTDIR}/simhits.pkl \
 #   --write-mds ${OUTDIR}/mds.pkl # --plot
 
+# How to make background dz plot for OTB L01 only
+# GEO="v01"
+# SMEAR="10um"
+# maia_doublets \
+#   --geo ${GEO} \
+#   --signal \
+#   --digi \
+#   --smear ${SMEAR} \
+#   --layers OTB0 OTB1 \
+#   --cutflow "" \
+#   --plot
+#   # --background10 \
