a7adad1a841125531bc0d02f22cb164d9886fb24
alexandertuna <alexander.tuna@gmail.com>
2026-08-19 17:53:19 -0700

    Little running scripts

--- run/run.sh
+++ run/run.sh
@@ -7,30 +7,31 @@ mkdir -p ${OUTPUT_DIR}
 
 # How to calibrate cut thresholds (3sigma intervals)
 # maia_doublets --geo v01 --signal --digi --smear 10um --calibrate
+# maia_doublets --geo v05 --signal --digi --smear 10um --calibrate
+# maia_doublets --geo v06 --signal --digi --smear 10um --calibrate
+# maia_doublets --geo v07 --signal --digi --smear 10um --calibrate
 
 # How to make plots
-GEO="v01"
-SMEAR="10um"
-PKL_DIR=${OUTPUT_DIR}/${GEO}_signal_digi_${SMEAR}
-maia_doublets \
-    --geo ${GEO} \
-    --signal \
-    --digi \
-    --smear ${SMEAR} \
-    --write-hits ${PKL_DIR}/hits.pkl \
-    --write-mcps ${PKL_DIR}/mcps.pkl \
-    --write-mds ${PKL_DIR}/mds.pkl \
-    --write-t2s ${PKL_DIR}/t2s.pkl \
-    --write-t4s ${PKL_DIR}/t4s.pkl \
-    --write-t8s ${PKL_DIR}/t8s.pkl \
-    --cut-mds \
-    --cut-t2s \
-    --cut-t4s \
-    --cut-t8s \
-    --plot
+# GEO="v06"
+# SMEAR="10um"
+# PKL_DIR=${OUTPUT_DIR}/${GEO}_signal_digi_${SMEAR}
+# maia_doublets \
+#     --geo ${GEO} \
+#     --signal \
+#     --digi \
+#     --smear ${SMEAR} \
+#     --write-hits ${PKL_DIR}/hits.pkl \
+#     --write-mcps ${PKL_DIR}/mcps.pkl \
+#     --write-mds ${PKL_DIR}/mds.pkl \
+#     --write-t2s ${PKL_DIR}/t2s.pkl \
+#     --write-t4s ${PKL_DIR}/t4s.pkl \
+#     --write-t8s ${PKL_DIR}/t8s.pkl \
+#     --plot
 
 # How to find the overall efficiency
-# maia_doublets --geo v01 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
+# maia_doublets --geo v05 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
+# maia_doublets --geo v06 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
+# maia_doublets --geo v07 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
 
 # How to plot the 0-10 GeV pionGun sample
 # maia_doublets --geo v01 -i "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/pionGun_pT_0_10/10um/pionGun_pT_0_10_digi_3*" --digi --smear 10um --plot
@@ -42,18 +43,18 @@ maia_doublets \
 # How to pickle the 0-10 GeV muonGun sample
 # GEO="v01"
 # SMEAR="10um"
-# OUTDIR=${GEO}_muonGun_pT_0_10_digi_${SMEAR}
+# PKL_DIR=${OUTPUT_DIR}/${GEO}_muonGun_pT_0_10_digi_${SMEAR}
 # maia_doublets \
 #     --geo ${GEO} \
 #     -i "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_0_10/10um/muonGun_pT_0_10_digi_3*" \
 #     --digi \
 #     --smear ${SMEAR} \
-#     --write-hits ${OUTDIR}/hits.pkl \
-#     --write-mcps ${OUTDIR}/mcps.pkl \
-#     --write-mds ${OUTDIR}/mds.pkl \
-#     --write-t2s ${OUTDIR}/t2s.pkl \
-#     --write-t4s ${OUTDIR}/t4s.pkl \
-#     --write-t8s ${OUTDIR}/t8s.pkl \
+#     --write-hits ${PKL_DIR}/hits.pkl \
+#     --write-mcps ${PKL_DIR}/mcps.pkl \
+#     --write-mds ${PKL_DIR}/mds.pkl \
+#     --write-t2s ${PKL_DIR}/t2s.pkl \
+#     --write-t4s ${PKL_DIR}/t4s.pkl \
+#     --write-t8s ${PKL_DIR}/t8s.pkl \
 #     --cut-mds \
 #     --cut-t2s \
 #     --cut-t4s \
@@ -61,19 +62,19 @@ maia_doublets \
 
 
 # How to run background neutrinoGun
-# GEO="v01"
-# SMEAR="10um"
-# OUTDIR=${GEO}_background100_digi_${SMEAR}
-# maia_doublets \
-#   --geo ${GEO} \
-#   --background100 \
-#   --digi \
-#   --smear ${SMEAR} \
-#   --fast-mds \
-#   --cutflow ${OUTDIR}/cutflow.ndjson \
-#   --write-mcps ${OUTDIR}/mcps.pkl \
-#   --write-hits ${OUTDIR}/hits.pkl \
-#   --write-mds ${OUTDIR}/mds.pkl # --plot
+GEO="v06"
+SMEAR="10um"
+PKL_DIR=${OUTPUT_DIR}/${GEO}_background100_digi_${SMEAR}
+maia_doublets \
+  --geo ${GEO} \
+  --background100 \
+  --digi \
+  --smear ${SMEAR} \
+  --fast-mds \
+  --cutflow ${PKL_DIR}/cutflow.ndjson \
+  --write-mcps ${PKL_DIR}/mcps.pkl \
+  --write-hits ${PKL_DIR}/hits.pkl \
+  --write-mds ${PKL_DIR}/mds.pkl # --plot
 
 # How to make background dz plot for OTB L01 only
 # GEO="v01"
