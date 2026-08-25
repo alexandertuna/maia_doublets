67c9391b2092239a05b48f836b29715efde14df7
alexandertuna <alexander.tuna@gmail.com>
2026-08-25 16:42:53 -0700

    Use neutrinoGun instead of background100 for consistency

--- run/run.sh
+++ run/run.sh
@@ -42,14 +42,14 @@ CMD="maia_doublets \
     --smear ${SMEAR} \
     --no-cuts \
     --stop-after-mds"
-time ${CMD} --write-mds ${PKL_DIR}/mds_ITB0_ITB1.pkl --layers ITB0 ITB1
-time ${CMD} --write-mds ${PKL_DIR}/mds_ITB2_ITB3.pkl --layers ITB2 ITB3
-time ${CMD} --write-mds ${PKL_DIR}/mds_ITB4_ITB5.pkl --layers ITB4 ITB5
-time ${CMD} --write-mds ${PKL_DIR}/mds_ITB6_ITB7.pkl --layers ITB6 ITB7
-time ${CMD} --write-mds ${PKL_DIR}/mds_OTB0_OTB1.pkl --layers OTB0 OTB1
-time ${CMD} --write-mds ${PKL_DIR}/mds_OTB2_OTB3.pkl --layers OTB2 OTB3
-time ${CMD} --write-mds ${PKL_DIR}/mds_OTB4_OTB5.pkl --layers OTB4 OTB5
-time ${CMD} --write-mds ${PKL_DIR}/mds_OTB6_OTB7.pkl --layers OTB6 OTB7
+# time ${CMD} --write-mds ${PKL_DIR}/mds_ITB0_ITB1.pkl --layers ITB0 ITB1
+# time ${CMD} --write-mds ${PKL_DIR}/mds_ITB2_ITB3.pkl --layers ITB2 ITB3
+# time ${CMD} --write-mds ${PKL_DIR}/mds_ITB4_ITB5.pkl --layers ITB4 ITB5
+# time ${CMD} --write-mds ${PKL_DIR}/mds_ITB6_ITB7.pkl --layers ITB6 ITB7
+# time ${CMD} --write-mds ${PKL_DIR}/mds_OTB0_OTB1.pkl --layers OTB0 OTB1
+# time ${CMD} --write-mds ${PKL_DIR}/mds_OTB2_OTB3.pkl --layers OTB2 OTB3
+# time ${CMD} --write-mds ${PKL_DIR}/mds_OTB4_OTB5.pkl --layers OTB4 OTB5
+# time ${CMD} --write-mds ${PKL_DIR}/mds_OTB6_OTB7.pkl --layers OTB6 OTB7
 
 # How to find the overall efficiency
 # maia_doublets --geo v05 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
@@ -85,22 +85,23 @@ time ${CMD} --write-mds ${PKL_DIR}/mds_OTB6_OTB7.pkl --layers OTB6 OTB7
 
 
 # How to run background neutrinoGun
-# GEO="v06"
-# SMEAR="10um"
-# PKL_DIR=${OUTPUT_DIR}/${GEO}_background100_digi_${SMEAR}
-# maia_doublets \
-#   --geo ${GEO} \
-#   --background100 \
-#   --digi \
-#   --smear ${SMEAR} \
-#   --cutflow ${PKL_DIR}/cutflow.ndjson \
-#   --read-mcps ${PKL_DIR}/mcps.pkl \
-#   --read-hits ${PKL_DIR}/hits.pkl \
-#   --read-mds ${PKL_DIR}/mds.pkl \
-#   --write-t2s ${PKL_DIR}/t2s.pkl \
-#   --write-t4s ${PKL_DIR}/t4s.pkl \
-#   --write-t8s ${PKL_DIR}/t8s.pkl \
-#   --fast-mds --plot
+GEO="v06"
+SMEAR="10um"
+PKL_DIR=${OUTPUT_DIR}/${GEO}_neutrinoGun_digi_${SMEAR}
+mkdir -p ${PKL_DIR}
+maia_doublets \
+  --geo ${GEO} \
+  --neutrinoGun \
+  --digi \
+  --smear ${SMEAR} \
+  --cutflow ${PKL_DIR}/cutflow.ndjson \
+  --write-mcps ${PKL_DIR}/mcps.pkl \
+  --write-hits ${PKL_DIR}/hits.pkl \
+  --write-mds ${PKL_DIR}/mds.pkl \
+  --write-t2s ${PKL_DIR}/t2s.pkl \
+  --write-t4s ${PKL_DIR}/t4s.pkl \
+  --write-t8s ${PKL_DIR}/t8s.pkl \
+  --fast-mds --plot
 
 # How to make background dz plot for OTB L01 only
 # GEO="v01"
@@ -117,10 +118,10 @@ time ${CMD} --write-mds ${PKL_DIR}/mds_OTB6_OTB7.pkl --layers OTB6 OTB7
 
 # How to make the scatter plot demo
 # time python ../maia_doublets/demos/demo_scatter2d.py \
-#     --hits v01_background100_digi_10um/hits_0.pkl \
-#     --mds v01_background100_digi_10um/mds_0.pkl \
-#     --t2s v01_background100_digi_10um/t2s_0.pkl \
-#     --t4s v01_background100_digi_10um/t4s_0.pkl \
-#     --t8s v01_background100_digi_10um/t8s_0.pkl \
+#     --hits v01_neutrinoGun_digi_10um/hits_0.pkl \
+#     --mds v01_neutrinoGun_digi_10um/mds_0.pkl \
+#     --t2s v01_neutrinoGun_digi_10um/t2s_0.pkl \
+#     --t4s v01_neutrinoGun_digi_10um/t4s_0.pkl \
+#     --t8s v01_neutrinoGun_digi_10um/t8s_0.pkl \
 #     --signal v01_signal_digi_10um/t8s.pkl \
 #     --output tmp.png
