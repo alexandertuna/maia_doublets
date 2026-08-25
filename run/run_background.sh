cde52e90b4ce6f87bdc2c44b95fc24d3685e04eb
alexandertuna <alexander.tuna@gmail.com>
2026-08-24 17:24:05 -0700

    Use pkl dir instead of output dir

--- run/run_background.sh
+++ run/run_background.sh
@@ -1,25 +1,28 @@
-GEO="v01"
+GEO="v07"
 SMEAR="10um"
-DATA_DIR="/ceph/users/atuna/work/maia/maia_noodling/samples/${GEO}/neutrinoGun/${SMEAR}"
-# neutrinoGun_digi_3.slcio
+DATA_DIR="/ceph/users/atuna/work/maia/maia_datasets/samples/${GEO}/neutrinoGun/${SMEAR}"
 
-for IT in $(seq 50 99); do
+OUTPUT_DIR=$(dirname "${BASH_SOURCE[0]}")/../output
+PKL_DIR=${OUTPUT_DIR}/${GEO}_background100_digi_${SMEAR}
+mkdir -p ${PKL_DIR}
+echo "Output directory: ${PKL_DIR}"
 
-    OUTDIR=${GEO}_background100_digi_${SMEAR}
+for IT in $(seq 10 19); do
 
+    echo "Running iteration ${IT} ..."
     maia_doublets \
     -i ${DATA_DIR}/neutrinoGun_digi_${IT}.slcio \
     --geo ${GEO} \
     --digi \
     --smear ${SMEAR} \
     --fast-mds \
-    --cutflow ${OUTDIR}/cutflow_${IT}.ndjson \
-    --write-mcps ${OUTDIR}/mcps_${IT}.pkl \
-    --write-hits ${OUTDIR}/hits_${IT}.pkl \
-    --write-mds ${OUTDIR}/mds_${IT}.pkl \
-    --write-t2s ${OUTDIR}/t2s_${IT}.pkl \
-    --write-t4s ${OUTDIR}/t4s_${IT}.pkl \
-    --write-t8s ${OUTDIR}/t8s_${IT}.pkl \
-    &> ${OUTDIR}/log_${IT}.txt
+    --cutflow ${PKL_DIR}/cutflow_${IT}.ndjson \
+    --write-mcps ${PKL_DIR}/mcps_${IT}.pkl \
+    --write-hits ${PKL_DIR}/hits_${IT}.pkl \
+    --write-mds ${PKL_DIR}/mds_${IT}.pkl \
+    --write-t2s ${PKL_DIR}/t2s_${IT}.pkl \
+    --write-t4s ${PKL_DIR}/t4s_${IT}.pkl \
+    --write-t8s ${PKL_DIR}/t8s_${IT}.pkl \
+    &> ${PKL_DIR}/log_${IT}.txt
 
 done
