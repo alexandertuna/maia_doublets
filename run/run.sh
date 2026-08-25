322e40d17c9796ee4e77534964e2ec3f3e2b4340
alexandertuna <alexander.tuna@gmail.com>
2026-08-24 17:23:29 -0700

    Add an example of writing all bib mds to disk

--- run/run.sh
+++ run/run.sh
@@ -12,9 +12,10 @@ mkdir -p ${OUTPUT_DIR}
 # maia_doublets --geo v07 --signal --digi --smear 10um --calibrate
 
 # How to make plots
-# GEO="v06"
+# GEO="v07"
 # SMEAR="10um"
 # PKL_DIR=${OUTPUT_DIR}/${GEO}_signal_digi_${SMEAR}
+# mkdir -p ${PKL_DIR}
 # maia_doublets \
 #     --geo ${GEO} \
 #     --signal \
@@ -27,6 +28,28 @@ mkdir -p ${OUTPUT_DIR}
 #     --write-t4s ${PKL_DIR}/t4s.pkl \
 #     --write-t8s ${PKL_DIR}/t8s.pkl \
 #     --plot
+
+# How to write background MDs to disk without cuts
+#   and without stressing the memory by writing all layers at once
+GEO="v07"
+SMEAR="10um"
+PKL_DIR=${OUTPUT_DIR}/${GEO}_neutrinoGun10_digi_${SMEAR}
+mkdir -p ${PKL_DIR}
+CMD="maia_doublets \
+    --geo ${GEO} \
+    --neutrinoGun10 \
+    --digi \
+    --smear ${SMEAR} \
+    --no-cuts \
+    --stop-after-mds"
+time ${CMD} --write-mds ${PKL_DIR}/mds_ITB0_ITB1.pkl --layers ITB0 ITB1
+time ${CMD} --write-mds ${PKL_DIR}/mds_ITB2_ITB3.pkl --layers ITB2 ITB3
+time ${CMD} --write-mds ${PKL_DIR}/mds_ITB4_ITB5.pkl --layers ITB4 ITB5
+time ${CMD} --write-mds ${PKL_DIR}/mds_ITB6_ITB7.pkl --layers ITB6 ITB7
+time ${CMD} --write-mds ${PKL_DIR}/mds_OTB0_OTB1.pkl --layers OTB0 OTB1
+time ${CMD} --write-mds ${PKL_DIR}/mds_OTB2_OTB3.pkl --layers OTB2 OTB3
+time ${CMD} --write-mds ${PKL_DIR}/mds_OTB4_OTB5.pkl --layers OTB4 OTB5
+time ${CMD} --write-mds ${PKL_DIR}/mds_OTB6_OTB7.pkl --layers OTB6 OTB7
 
 # How to find the overall efficiency
 # maia_doublets --geo v05 --signal --digi --smear 10um --cut-mds --cut-t2s --cut-t4s --cut-t8s --plot
@@ -62,19 +85,22 @@ mkdir -p ${OUTPUT_DIR}
 
 
 # How to run background neutrinoGun
-GEO="v06"
-SMEAR="10um"
-PKL_DIR=${OUTPUT_DIR}/${GEO}_background100_digi_${SMEAR}
-maia_doublets \
-  --geo ${GEO} \
-  --background100 \
-  --digi \
-  --smear ${SMEAR} \
-  --fast-mds \
-  --cutflow ${PKL_DIR}/cutflow.ndjson \
-  --write-mcps ${PKL_DIR}/mcps.pkl \
-  --write-hits ${PKL_DIR}/hits.pkl \
-  --write-mds ${PKL_DIR}/mds.pkl # --plot
+# GEO="v06"
+# SMEAR="10um"
+# PKL_DIR=${OUTPUT_DIR}/${GEO}_background100_digi_${SMEAR}
+# maia_doublets \
+#   --geo ${GEO} \
+#   --background100 \
+#   --digi \
+#   --smear ${SMEAR} \
+#   --cutflow ${PKL_DIR}/cutflow.ndjson \
+#   --read-mcps ${PKL_DIR}/mcps.pkl \
+#   --read-hits ${PKL_DIR}/hits.pkl \
+#   --read-mds ${PKL_DIR}/mds.pkl \
+#   --write-t2s ${PKL_DIR}/t2s.pkl \
+#   --write-t4s ${PKL_DIR}/t4s.pkl \
+#   --write-t8s ${PKL_DIR}/t8s.pkl \
+#   --fast-mds --plot
 
 # How to make background dz plot for OTB L01 only
 # GEO="v01"
