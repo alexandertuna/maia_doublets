ab4a64a6c8a7a961cc69cfec0c2c88a253fe594f
alexandertuna <alexander.tuna@gmail.com>
2026-09-14 17:58:06 -0700

    Add 20um v06 data

--- maia_doublets/datasets.py
+++ maia_doublets/datasets.py
@@ -287,6 +287,18 @@ filepaths["muonGun_pT_2p0_2p1"] = {
         # "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/10um/muonGun_pT_2p0_2p1_digi_348.slcio",
         # "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/10um/muonGun_pT_2p0_2p1_digi_349.slcio",
     ],
+    ("v06", "digi", "20um"): [
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_300.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_301.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_302.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_303.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_304.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_305.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_306.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_307.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_308.slcio",
+        "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/20um/muonGun_pT_2p0_2p1_digi_309.slcio",
+    ],
     ("v06", "digi", "30um"): [
         "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/30um/muonGun_pT_2p0_2p1_digi_300.slcio",
         "/ceph/users/atuna/work/maia/maia_datasets/samples/v06/muonGun_pT_2p0_2p1/30um/muonGun_pT_2p0_2p1_digi_301.slcio",
